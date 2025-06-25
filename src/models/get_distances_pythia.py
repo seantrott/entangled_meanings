15021843e26cb8e3deed9546ba234de427d3aff4
Sean <trott.sean@gmail.com>
2025-06-25 14:31:45 -0700

    410mg

--- src/models/get_distances_pythia.py
+++ src/models/get_distances_pythia.py
@@ -22,13 +22,13 @@ import utils
 ### Models to test
 MODELS = [
          # 'EleutherAI/pythia-14m',
-         'EleutherAI/pythia-70m',
-         'EleutherAI/pythia-160m',
-         'EleutherAI/pythia-410m',
-          'EleutherAI/pythia-1b',
-          #  'EleutherAI/pythia-1.4b',
-          # 'EleutherAI/pythia-2.8b',
-          # 'EleutherAI/pythia-6.9b',
+         # 'EleutherAI/pythia-70m',
+         # 'EleutherAI/pythia-160m',
+         # 'EleutherAI/pythia-410m',
+         # 'EleutherAI/pythia-1b',
+         'EleutherAI/pythia-1.4b',
+         'EleutherAI/pythia-2.8b',
+         'EleutherAI/pythia-6.9b',
           # 'EleutherAI/pythia-12b',
           ]
 
