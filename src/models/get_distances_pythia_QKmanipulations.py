60f67c85b7f40acb9981b1fcaad74d84754576d8
Sean <trott.sean@gmail.com>
2025-07-03 18:47:08 -0700

    distances

--- src/models/get_distances_pythia_QKmanipulations.py
+++ src/models/get_distances_pythia_QKmanipulations.py
@@ -127,7 +127,7 @@ def main(df, mpath, revisions, modification, layer_indices, head_indices):
             outside_premod = premod_qkv_weight[mask, :]
             outside_postmod = qkv_weight[mask, :]
 
-            if np.array_equal(block_premod,block_postmod) and modification != "ablate_copy_step1": 
+            if np.array_equal(block_premod,block_postmod) and (modification != "ablate_copy_step1" or step != 0):
                 raise ValueError("The target matrix blocks are identical! They should be different.")
 
             if not np.array_equal(outside_premod, outside_postmod):
